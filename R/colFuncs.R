ceee332b62f04c247553cf6548babc1347b4b793
Angus Wright <awright@astro.ruhr-uni-bochum.de>
2020-08-19 12:10:53 +0200

    added colFuncs, improved split.expr

---
+++ R/colFuncs.R
@@ -0,0 +1,125 @@
+colWeightedCounts<-function (x, w, rows = NULL, cols = NULL, value = TRUE, na.rm = FALSE, 
+    dim. = dim(x), ...) 
+{
+    if (is.matrix(x)) {
+    }
+    else if (is.vector(x)) {
+    }
+    else {
+        stop("Argument 'x' must be a matrix or a vector: ", mode(x)[1L])
+    }
+    dim. <- as.integer(dim.)
+    if (length(value) != 1L) {
+        stop("Argument 'value' has to be a single value: ", length(value))
+    }
+    storage.mode(value) <- storage.mode(x)
+    #if (is.numeric(x) || is.logical(x)) {
+    #    na.rm <- as.logical(na.rm)
+    #    has_nas <- TRUE
+    #    counts <- .Call(C_colCounts, x, dim., rows, cols, value, 
+    #        2L, na.rm, has_nas)
+    #}
+    #else {
+        if (is.vector(x)) 
+            dim(x) <- dim.
+        if (!is.null(rows) && !is.null(cols)) 
+            x <- x[rows, cols, drop = FALSE]
+        else if (!is.null(rows)) 
+            x <- x[rows, , drop = FALSE]
+        else if (!is.null(cols)) 
+            x <- x[, cols, drop = FALSE]
+        dim. <- dim(x)
+        if (is.na(value)) {
+            counts <- apply(x, MARGIN = 2L, FUN = function(x) sum(is.na(x)))
+        }
+        else if (!missing(w)) { 
+            #if (length(x)!=length(w)) { 
+            #  print(str(x)) 
+            #  print(str(w))
+            #  #stop("weight and index arrays differ") 
+            #} 
+            counts <- apply(x, MARGIN = 2L, FUN = function(x) {
+              if (length(x)!=length(w)) { 
+                stop(paste("weight and index arrays differ:\n",length(x),"!=",length(w)))
+              } else {
+                sum(w[x == value], na.rm = na.rm)}} )
+        }
+        else {
+            counts <- apply(x, MARGIN = 2L, FUN = function(x) sum(x == 
+                value, na.rm = na.rm))
+        }
+    #}
+    if (missing(w)) { 
+      as.integer(counts)
+    } else { 
+      as.numeric(counts)
+    }
+}
+colWeightedTabulates<-function (x, w, rows = NULL, cols = NULL, values = NULL, cores=1, ...) 
+{
+    library(foreach)
+    if (cores>1) { 
+      library(doParallel)
+      registerDoParallel(cores=cores)
+    }
+
+    if (is.integer(x)) {
+    }
+    else if (is.logical(x)) {
+    }
+    else if (is.raw(x)) {
+    }
+    else {
+        stop("Argument 'x' is not of type integer or raw: ", 
+            class(x)[1])
+    }
+    if (!is.null(rows) && !is.null(cols)) 
+        x <- x[rows, cols, drop = FALSE]
+    else if (!is.null(rows)) 
+        x <- x[rows, , drop = FALSE]
+    else if (!is.null(cols)) 
+        x <- x[, cols, drop = FALSE]
+    if (is.null(values)) {
+        values <- as.vector(x)
+        values <- unique(values)
+        if (is.raw(values)) {
+            values <- as.integer(values)
+            values <- sort(values)
+            names <- sprintf("%x", values)
+            names <- paste("0x", names, sep = "")
+            values <- as.raw(values)
+        }
+        else {
+            values <- sort(values, na.last = TRUE)
+            names <- as.character(values)
+        }
+    }
+    else {
+        if (is.raw(values)) {
+            names <- sprintf("%x", as.integer(values))
+            names <- paste("0x", names, sep = "")
+        }
+        else {
+            names <- as.character(values)
+        }
+    }
+    transpose <- FALSE
+    if (!transpose) {
+        nbr_of_values <- length(values)
+        counts <- matrix(0L, nrow = ncol(x), ncol = nbr_of_values)
+        colnames(counts) <- names
+        if (missing(na.rm)) {
+          na.rm <- matrixStats::anyMissing(x)
+        }
+        if (!missing(w)) { 
+          counts<-foreach(value=values,.combine='cbind',.export=c('x','w'))%dopar% {
+              return=colWeightedCounts(x, w, value = value, na.rm = na.rm)
+          }
+        } else { 
+          counts<-foreach(value=values,.combine='cbind',.export=c('x'))%dopar% {
+              return=colWeightedCounts(x, value = value, na.rm = na.rm)
+          }
+        }
+    }
+    counts
+}
--- R/split.expr.R
+++ R/split.expr.R
@@ -2,7 +2,8 @@
 # Function strips hanging brackets from text expressions
 #
 
-split.expr<-function(expr,splitchar='+-=/*^~<>()',outvar="V",ignore=c("abs","sqrt")) { 
+split.expr<-function(expr,splitchar='+-=/*^~<>()&!|,',outvar="V",
+                      matchchars=rbind(c("[[","]]")),ignore=c("abs","sqrt",'na.rm')) { 
   if (grepl("#",expr)) stop("expr cannot have hashes (#) in it!") 
   #Strip whitespace from expression
   expr<-gsub(" ","",expr)
@@ -17,21 +18,32 @@ split.expr<-function(expr,splitchar='+-=/*^~<>()',outvar="V",ignore=c("abs","sqr
   comps<-helpRfuncs::vecsplit(expr.tmp,' ')
   #remove empty comoponents
   comps<-comps[which(comps!="")]
+  #Check for broken array subsets {{{ 
+  combset<-NULL
+  for (match.ind in 1:nrow(matchchars)) {  
+    combset<-.find.match(matchchars[match.ind,],comps,returnAll=FALSE)
+    comps<-.find.match(matchchars[match.ind,],comps)
+  }
+  #}}}
   #Get the classes of the components
   classes<-unlist(lapply(comps,FUN=function(X) class(try(eval(parse(text=X)),silent=T))))
   if (any(classes=='function')) { 
     close.brace<-0
-    combset<-NULL
-    for (i in which(classes=='function')) {
-      #If the last close.brace was after this function, skip it
-      if (close.brace>i) next
-      #combine functions into single components
-      close.brace<-i+which(cumsum(ifelse(grepl("\\(",comps[-(1:i)]),1,0)+
-                                  ifelse(grepl("\\)",comps[-(1:i)]),-1,0))==0)[1]
-      if (is.na(close.brace)) stop("There is unmatched braces in expression!")
-      comb<-paste(comps[i:close.brace],collapse="")
-      combset<-c(combset,comb)
+    func.ind<-which(classes=="function")
+    #Merge the functions with their trailing bracket
+    for (i in rev(func.ind)) { 
+      if (i==1 & length(comps)>=i+2) { 
+        comps<-c(paste0(comps[i],comps[i+1]),comps[(i+2):length(comps)])
+      } else if (i==1) { 
+        comps<-paste0(comps[i],comps[i+1])
+      } else if (length(comps)==i+1) { 
+        comps<-c(comps[1:(i-1)],paste0(comps[i],comps[i+1]))
+      } else {
+        comps<-c(comps[1:(i-1)],paste0(comps[i],comps[i+1]),comps[(i+2):length(comps)])
+      }
+      classes<-classes[-(i+1)]
     }
+    combset<-c(combset,.find.match(c("(",")"),comps,subset=which(classes=='function'),returnAll=FALSE,nestOrder=TRUE))
     #remove the functions from the strings
     expr.tmp<-expr
     for (i in 1:length(combset)) { 
@@ -45,9 +57,26 @@ split.expr<-function(expr,splitchar='+-=/*^~<>()',outvar="V",ignore=c("abs","sqr
       expr.tmp<-gsub(paste0("\\",char),paste0(" "),expr.tmp,perl=T)
     #Split everything on spaces 
     comps<-helpRfuncs::vecsplit(expr.tmp,' ')
-    comps<-comps[which(comps!="")]
+    if (any(comps=="")) {
+      comps<-comps[which(comps!="")]
+    }
+    #Check for lone numbers
     classes<-unlist(lapply(comps,FUN=function(X) class(try(eval(parse(text=X)),silent=T))))
-    if (any(classes=='function')) { stop("function removal failed") } 
+    if (any(classes=="logical")) { 
+      #Some of the components are just numbers
+      comps<-comps[which(classes!="logical")]
+      classes<-unlist(lapply(comps,FUN=function(X) class(try(eval(parse(text=X)),silent=T))))
+    }
+    if (any(classes=="numeric")) { 
+      #Some of the components are just numbers
+      comps<-comps[which(classes!="numeric")]
+      classes<-unlist(lapply(comps,FUN=function(X) class(try(eval(parse(text=X)),silent=T))))
+    }
+    #Remove any wrapping functions 
+    if (any(classes=="function")) { 
+      comps<-comps[which(classes!='function')]
+      classes<-unlist(lapply(comps,FUN=function(X) class(try(eval(parse(text=X)),silent=T))))
+    }
     comps<-c(combset,comps)
   }
   expr.tmp<-expr
@@ -56,4 +85,66 @@ split.expr<-function(expr,splitchar='+-=/*^~<>()',outvar="V",ignore=c("abs","sqr
   }
   names(comps)<-paste0(outvar,1:length(comps))
   return=list(components=comps,replace.expr=expr.tmp)
+}
+
+.find.match<-function(matchchars,comps,subset,returnAll=TRUE,nestOrder=FALSE) {
+  if (length(matchchars)>2)  
+    stop("expecting two sets of characters to match") 
+  match.open<-matchchars[1]
+  match.close<-matchchars[2]
+  #Count the number of brace sets per component
+  close.brace<-open.brace<-rep(0,length(comps))
+  tmp.comps<-comps
+  while (any(grepl(match.open,tmp.comps,fixed=T)|grepl(match.close,tmp.comps,fixed=T))) {
+    open.brace<-open.brace+ifelse(grepl(match.open,tmp.comps,fixed=T),1,0)
+    close.brace<-close.brace+ifelse(grepl(match.close,tmp.comps,fixed=T),1,0)
+    tmp.comps<-sub(match.open,'',tmp.comps,fixed=T)
+    tmp.comps<-sub(match.close,'',tmp.comps,fixed=T)
+  }
+  if (sum(open.brace)!=sum(close.brace)) { 
+    stop(paste0("There are mis-matched brackets ",match.open,"...",match.close,":\n",paste(comps,collapse=''))) 
+  }
+  if (any(open.brace!=close.brace)) { 
+    open.brace<-cumsum(open.brace)
+    close.brace<-cumsum(close.brace)
+    new.comps<-NULL
+    running.end<-tmp.close<-0
+    if (missing(subset)) { 
+      subset<-1:length(open.brace)
+    }
+    if (nestOrder) { 
+      subset<-subset[order(decreasing=TRUE,(open.brace-close.brace)[subset])]
+      returnAll<-FALSE
+    }
+    for (start in subset) {
+      #If the last close.brace was after this function, skip it
+      if (tmp.close>=start & !nestOrder) next
+      if ((open.brace-close.brace)[start]==0) { 
+        if (returnAll) { 
+          new.comps<-c(new.comps,comps[start])
+          running.end<-start
+        }
+        next
+      }
+      #combine functions into single components
+      tmp.close<-start+which((open.brace-close.brace)[-(1:start)]==(open.brace-close.brace)[start]-1)[1]
+      if (is.na(tmp.close)) stop("There is unmatched braces in expression!")
+      join<-paste(comps[start:tmp.close],collapse="")
+      if (running.end!=(start-1) && returnAll) {
+        new.comps<-c(new.comps,comps[(running.end+1):(start-1)])
+      }
+      new.comps<-c(new.comps,join)
+      running.end<-tmp.close
+    }
+    if (running.end<length(open.brace) && returnAll) {
+      new.comps<-c(new.comps,comps[(running.end+1):length(open.brace)])
+    }
+    return=new.comps
+  } else { 
+    if (returnAll) { 
+      return=comps
+    } else { 
+      return=NULL
+    }
+  }
 }
