66f68d412dfb3f7341a781a35ea58bd1fa7f0859
Angus H Wright <awright@astro.rub.de>
2023-07-24 11:35:20 +0200

    bugfix in plot.chain

--- R/plot.chain.R
+++ R/plot.chain.R
@@ -3,12 +3,12 @@
 # File Name : plot.chain.R
 # Created By : awright
 # Creation Date : 24-07-2023
-# Last Modified : Mon 24 Jul 2023 11:33:41 AM CEST
+# Last Modified : Mon 24 Jul 2023 11:35:10 AM CEST
 #
 #=========================================
 
 plot.chain<-function(chain,colkeep="COSMOL|weight",coldel="COSMOMC",labelrm="COSMOLOGICAL_PARAMETERS--",...) { 
-  chain<-chain[,grepl(colkeep,colnames(chain)&!grepl(coldel,colnames(chain))),with=F]
+  chain<-chain[,grepl(colkeep,colnames(chain))&!grepl(coldel,colnames(chain)),with=F]
   colnames(chain)<-gsub(labelrm,"",colnames(chain))
   res=helpRfuncs::triplot(chain,...)
   return=res
